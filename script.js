3095d2e20e4978d50d77e94fd2e55e2ccb0074ed
Jacksonalves95 <jackson.alves1995@gmail.com>
2025-06-06 00:28:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem() {
+  const mensagemDiv = document.getElementById("mensagem");
+  mensagemDiv.innerHTML = "<p>Olá, Bem-vindo a minha página!</p>";
+  mensagemDiv.style.backgroundColor = "#ffcc00";
+  mensagemDiv.style.padding = "10px";
+  mensagemDiv.style.textAlign = "center";
+}
